6dee358737f96525a66f076b2f2fba9cdcb653d0
Sara28himmich <sara.himmich@epitech.eu>
2025-10-28 15:01:05 +0100

    [+] step 2

--- new_project/src/ERC20.sol
+++ new_project/src/ERC20.sol
@@ -1,8 +1,24 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.20;
+
 import "./interface/IERC20.sol";
 
 contract ERC20 is IERC20 {
-     
+    string public name;
+    string public symbol;
+    uint8 public constant decimals = 18;
+
+    uint256 private _totalSupply;
+
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    constructor(string memory name_, string memory symbol_, uint256 initialSupply) {
+        name = name_;
+        symbol = symbol_;
+        _totalSupply = initialSupply;
+        _balances[msg.sender] = initialSupply;
+        emit Transfer(address(0), msg.sender, initialSupply);
+    }
+
 }
