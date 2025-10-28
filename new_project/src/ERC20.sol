756ea754a1f81456dee4c2ca97e62a507df6209f
Sara28himmich <sara.himmich@epitech.eu>
2025-10-28 15:31:38 +0100

    [+] step 3

--- new_project/src/ERC20.sol
+++ new_project/src/ERC20.sol
@@ -21,4 +21,54 @@ contract ERC20 is IERC20 {
         emit Transfer(address(0), msg.sender, initialSupply);
     }
 
+    /* IERC20 implementation */
+
+    function totalSupply() external view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address to, uint256 value) external override returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function allowance(address owner, address spender) external view override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 value) external override returns (bool) {
+        _approve(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) external override returns (bool) {
+        uint256 currentAllowance = _allowances[from][msg.sender];
+        require(currentAllowance >= value, "ERC20: transfer amount exceeds allowance");
+        _approve(from, msg.sender, currentAllowance - value);
+        _transfer(from, to, value);
+        return true;
+    }
+
+    /* Internal helpers */
+    function _transfer(address from, address to, uint256 value) internal {
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+        uint256 fromBalance = _balances[from];
+        require(fromBalance >= value, "ERC20: transfer amount exceeds balance");
+        _balances[from] = fromBalance - value;
+        _balances[to] += value;
+        emit Transfer(from, to, value);
+    }
+
+    function _approve(address owner, address spender, uint256 value) internal {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+        _allowances[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
+
 }
